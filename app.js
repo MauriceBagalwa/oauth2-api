bfd59e2037e9d3614683b23270a168e5c99f1dfe
MauriceBagalwa <mauricebagalwa009@gmail.com>
2023-02-18 12:39:15 +0100

    test pm2 (12) aupdate package.jon file

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.status(200).send({
     status: true,
-    message: "✌ welcome to devops webHooks library - 29 - 🐱‍🚀",
+    message: "✌ welcome to devops webHooks library - 30 - 🐱‍🚀",
     data: {
       essaie: 1,
     },
