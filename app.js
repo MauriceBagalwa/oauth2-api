8fc7261c1933fd5c43564b35eca593613b59eb82
MauriceBagalwa <mauricebagalwa009@gmail.com>
2023-06-09 06:49:56 +0200

    bheti test deploy 005

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 app.get("/", (req, res) => {
   res.status(200).send({
     status: true,
-    message: "✌ welcome to devops webHooks library - auth 004 - 🐱‍🚀",
+    message: "✌ welcome to devops webHooks library - auth 005 - 🐱‍🚀",
     data: {
       essaie: 2,
     },
